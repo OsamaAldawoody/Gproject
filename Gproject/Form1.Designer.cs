3c7b1a5d86b600b619ad68f12d6c83bafdc0c146
OsamaAldawoody <frouzen1650@gmail.com>
2020-02-23 10:17:59 +0200

    Initial commit

---
+++ Gproject/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Gproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtZ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(51, 39);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(62, 20);
+            this.txtX.TabIndex = 0;
+            this.txtX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtX_KeyPress);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(141, 91);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(154, 39);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(62, 20);
+            this.txtY.TabIndex = 3;
+            this.txtY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtY_KeyPress);
+            // 
+            // txtZ
+            // 
+            this.txtZ.Location = new System.Drawing.Point(257, 39);
+            this.txtZ.Name = "txtZ";
+            this.txtZ.Size = new System.Drawing.Size(62, 20);
+            this.txtZ.TabIndex = 4;
+            this.txtZ.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtZ_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Z:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 169);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtZ);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtX);
+            this.Name = "Form1";
+            this.Text = "Telepresence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Gproject/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Gproject
+{
+    public partial class Form1 : Form
+    {
+        private static string path = @"D:\json\position.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (!(txtX.Text == ""|| txtY.Text == ""||txtZ.Text == ""))
+            {
+                Position position = new Position(int.Parse(txtX.Text.Trim()), int.Parse(txtY.Text.Trim()), int.Parse(txtZ.Text.Trim()));
+
+                serializeObjectAndSaveItInFile((Position)position, path);
+            }
+            else
+            {
+                showWarningMessage();
+            }    
+        }
+        
+
+        private void txtX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            allowDigitsOnly(sender, e);
+        }
+
+        private void txtY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            allowDigitsOnly(sender, e);
+        }
+
+        private void txtZ_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            allowDigitsOnly(sender, e);
+        }
+
+       
+        public void showWarningMessage()
+        {
+            MessageBox.Show("Enter Coordinates", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public void showSuccessfulMessage()
+        {
+            MessageBox.Show("File Saved !", "Finished", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void serializeObjectAndSaveItInFile(object obj,string path)
+        {
+            string jsonResult = JsonConvert.SerializeObject(obj);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(jsonResult.ToString());
+                    tw.Close();
+                }
+                showSuccessfulMessage();
+            }
+            else if (!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(jsonResult.ToString());
+                    tw.Close();
+                }
+                showSuccessfulMessage();
+            }
+        }
+
+        public void allowDigitsOnly(object sender,KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == '-')))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+    
+}
